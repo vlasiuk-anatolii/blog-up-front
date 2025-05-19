db6603ed72677bac0e260834f45b4e91b1f9ebdd
Vlasiuk Anatolii <anatolii.vlasiuk@bndigital.co>
2025-05-19 22:27:33 +0300

    fix: was changed next.config file

--- next.config.ts
+++ next.config.ts
@@ -21,14 +21,6 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'http://blog-up-backend-v6-env.eba-mamhw7rc.eu-north-1.elasticbeanstalk.com/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
